67625ffcf5bff50391998ad1244ec8fa915f9fcb
luukvdweel <6040961@mborijnland.nl>
2024-06-20 10:04:51 +0200

    pictures upload

--- javascript/value.js
+++ javascript/value.js
@@ -33,3 +33,19 @@ function multiplier() {
     document.getElementById("score").innerHTML = score + "D";
   }
 }
+
+// upload images 
+function uploadImage() {
+  const fileInput = document.getElementById('imageUpload');
+  const file = fileInput.files[0];
+  const reader = new FileReader();
+
+  reader.onload = function(e) {
+      const logoImage = document.getElementById('logoImage');
+      logoImage.src = e.target.result;
+  }
+
+  if (file) {
+      reader.readAsDataURL(file);
+  }
+}
